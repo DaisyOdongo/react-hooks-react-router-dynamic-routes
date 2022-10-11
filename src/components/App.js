37f33e12ccdcb4c80cb109a39e9509f4800728fb
+ <+>
2022-10-11 13:14:35 +0300

    assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route} from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
@@ -13,14 +13,14 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
+      {/* <Routes> */}
         <Route path="/movies">
           <MoviesPage movies={movies} />
         </Route>
         <Route exact path="/">
           <div>Home</div>
         </Route>
-      </Routes>
+      {/* </Routes> */}
     </div>
   );
 }
